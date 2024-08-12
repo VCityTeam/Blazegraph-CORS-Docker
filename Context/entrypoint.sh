f6a997a2ef336d7e039bfd32ca59e2e7f37c7d97
Jey Puget Gil <LinkJerome@gmail.com>
2024-08-12 14:41:26 +0200

    FIX: Update Dockerfile and entrypoint.sh for Blazegraph quad mode

--- Context/entrypoint.sh
+++ Context/entrypoint.sh
@@ -8,28 +8,22 @@ adduser blazegraph blazegraph
 chown -R blazegraph:blazegraph $BLAZEGRAPH_RW_PATH
 chown -R blazegraph:blazegraph /data
 
-sed "s/@@TIMEOUT@@/$BLAZEGRAPH_TIMEOUT/" $BLAZEGRAPH_RW_PATH/readonly_cors.tmp.xml | sed "s/@@READONLY@@/$BLAZEGRAPH_READONLY/" > /data/readonly_cors.xml
+sed "s/@@TIMEOUT@@/$BLAZEGRAPH_TIMEOUT/" $BLAZEGRAPH_RW_PATH/readonly_cors.tmp.xml | sed "s/@@READONLY@@/$BLAZEGRAPH_READONLY/" >/data/readonly_cors.xml
 
 # If quads environment variable is set, then we need to use the blazegraph-quads.properties file
 # Otherwise, we use the blazegraph.properties file
-if [ -z "$BLAZEGRAPH_QUADS" ]; then
-    echo "Starting Blazegraph in triple mode."
-    su-exec blazegraph:blazegraph \
-        java -Xmx$BLAZEGRAPH_MEMORY \
-        -Dfile.encoding=UTF-8 \
-        -Djetty.port=8080 \
-        -Djetty.overrideWebXml=readonly_cors.xml \
-        -Dbigdata.propertyFile=blazegraph.properties \
-        -cp $BLAZEGRAPH_RW_PATH/blazegraph.jar:$BLAZEGRAPH_RW_PATH/jetty-servlets-9.2.3.v20140905.jar \
-        com.bigdata.rdf.sail.webapp.StandaloneNanoSparqlServer
-else
+if [ "$BLAZEGRAPH_QUADS" = "true" ]; then
     echo "Starting Blazegraph in quad mode."
-    su-exec blazegraph:blazegraph \
-        java -Xmx$BLAZEGRAPH_MEMORY \
-        -Dfile.encoding=UTF-8 \
-        -Djetty.port=8080 \
-        -Djetty.overrideWebXml=readonly_cors.xml \
-        -Dbigdata.propertyFile=blazegraph-quads.properties \
-        -cp $BLAZEGRAPH_RW_PATH/blazegraph.jar:$BLAZEGRAPH_RW_PATH/jetty-servlets-9.2.3.v20140905.jar \
-        com.bigdata.rdf.sail.webapp.StandaloneNanoSparqlServer
+    mv /data/blazegraph-quads.properties /data/blazegraph.properties
+else
+    echo "Starting Blazegraph in triple mode."
 fi
+
+su-exec blazegraph:blazegraph \
+    java -Xmx$BLAZEGRAPH_MEMORY \
+    -Dfile.encoding=UTF-8 \
+    -Djetty.port=8080 \
+    -Djetty.overrideWebXml=readonly_cors.xml \
+    -Dbigdata.propertyFile=blazegraph.properties \
+    -cp $BLAZEGRAPH_RW_PATH/blazegraph.jar:$BLAZEGRAPH_RW_PATH/jetty-servlets-9.2.3.v20140905.jar \
+    com.bigdata.rdf.sail.webapp.StandaloneNanoSparqlServer
